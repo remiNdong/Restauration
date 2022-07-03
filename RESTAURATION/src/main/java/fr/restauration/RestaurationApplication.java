207b5c6741ac522f92660633a24bad826b7e92ba
remiNdong <ndongremi@aol.fr>
2022-07-03 16:09:00 +0200

    First commit

---
+++ RESTAURATION/src/main/java/fr/restauration/RestaurationApplication.java
@@ -0,0 +1,13 @@
+package fr.restauration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurationApplication.class, args);
+	}
+
+}
---
+++ RESTAURATION/src/test/java/fr/restauration/RestaurationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.restauration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
