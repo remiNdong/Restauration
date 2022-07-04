3e04e2c3ea210ba4675d8b882813e5324a92ce8d
remiNdong <ndongremi@aol.fr>
2022-07-04 20:21:30 +0200

    ajout test au cas ou constructeur facade echoue

--- RESTAURATION/src/test/java/fr/restauration/utils/RestaurantFacadeImplTest.java
+++ RESTAURATION/src/test/java/fr/restauration/utils/RestaurantFacadeImplTest.java
@@ -7,11 +7,42 @@ import org.junit.jupiter.api.Test;
 import fr.restauration.model.Restaurant;
 
 class RestaurantFacadeImplTest {
+	
+	/*Test du constructeur par defaut
+	 * recuperation du fichier Json via une requete Http
+	 * creation de la Map des Restaurants 
+	 */
 
 	@Test
 	void test() {
 		try {
 			RestaurantFacadeI facade=new RestaurantFacadeImpl();
+			assertTrue(facade.getListeRestaurants().size()>0);
+			assertEquals(facade.getRestaurant("257af888dc2719b01850e5f6752bbfc0c71d3ede").getNom_restaurant(),"CLIGNANCOURT");
+			
+			for(Restaurant resto : facade.getListeRestaurants())
+				System.out.println(resto);
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/*Test au cas ou  constructeur par defaut echoue
+	 * recuperation du fichier Json sur le disque
+	 * creation de la Map des Restaurants 
+	 */
+	
+	@Test
+	void test2() {
+		try {
+			RestaurantFacadeImpl facade=new RestaurantFacadeImpl();
+			facade.setParser(new JacksonParserRestaurant(facade.restaurer("restaurants.txt"))); 
+			facade.getParser().setCommand(facade.getAddCommand());
+			facade.getParser().parse();
 			assertTrue(facade.getListeRestaurants().size()>0);
 			assertEquals(facade.getRestaurant("257af888dc2719b01850e5f6752bbfc0c71d3ede").getNom_restaurant(),"CLIGNANCOURT");
 			
