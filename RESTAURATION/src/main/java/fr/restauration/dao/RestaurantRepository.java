d1a6f95f7610aed3ea5181d86ca9f2e75c26306a
remiNdong <ndongremi@aol.fr>
2022-07-15 19:44:23 +0200

    commencement du filtre

--- RESTAURATION/src/main/java/fr/restauration/dao/RestaurantRepository.java
+++ RESTAURATION/src/main/java/fr/restauration/dao/RestaurantRepository.java
@@ -1,10 +1,16 @@
 package fr.restauration.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import fr.restauration.model.Restaurant;
 
 public interface RestaurantRepository extends CrudRepository<Restaurant, String>{
 	
 	public Iterable<Restaurant> findByVille(String ville);
+	
+	
+	
+
 }
